1bab6310d0bf9e88bd1a451d07a6dcc526b6a4c0
rishabh <rishabh.sisodiya4@gmail.com>
2019-01-22 11:14:55 +0530

    Update method

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _= require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -62,6 +63,29 @@ app.delete('/todos/:id', (req,res) => {
     res.status(200).send({todo});
     console.log({todo});
   }).catch((err) => res.status(400).send('Id is wrong'));
+});
+
+app.patch('/todos/:id',(req,res) => {
+  var id=req.params.id;
+  var body= _.pick(req.body, ['text','completed']);
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send('Id is invalid');
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completeAt= new Date().getTime();
+  } else {
+    body.completed=false;
+    body.completeAt=null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body},{new:true}).then((todo) => {
+    if (!todo) {
+      return res.status(400).send();
+    }
+    res.status(200).send({todo});
+  }).catch((err) => { res.status(400).send()});
 });
 
 app.listen(port, () => {
